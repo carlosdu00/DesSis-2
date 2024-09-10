795758d3b8fce4377ae5296f1a24693058effb0e
carlodu00 <carlosdu.carloseduardo@gmail.com>
2024-09-10 20:29:17 -0300

    First upload

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
+  const name = "Prof. Felipe Becker Nunes";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{ uri: "https://faculdadeam.edu.br/favicon.ico" }}
+        style={{ width: 250, height: 250 }}
+      />
+
+      <Text style={styles.title}>Hello World!</Text>
+      <Text>Meu primeiro App!</Text>
+
+      <Text>{name}</Text>
     </View>
   );
 }
@@ -13,8 +21,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "red",
   },
 });
